84f1d76bc0d09f3b0ed1166047553a1a460ca9fa
lies046 <skytree.skysky@gmail.com>
2020-07-07 11:50:23 +0900

    Add Destroy

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -8,5 +8,10 @@
   </div>
   <div>
     <a href={{ route('shop.list') }}>一覧に戻る</a>
+    | <a href={{ route('shop.edit', ['id' => $shop->id])}}>編集</a>
+    <p></p>
+    {{ Form::open(['method' => 'delete', 'route' => ['shop.destroy', $shop->id]])}}
+      {{ Form::submit('削除', ['class' => 'btn btn-outline-danger']) }}
+    {{ Form::close()}}
   </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 Route::get('/shops', 'ShopController@index')->name('shop.list');
 Route::get('/shop/new', 'ShopController@create')->name('shop.new');
 Route::post('/shop', 'ShopController@store')->name('shop.store');
-Route::get('/shop/{id}', 'ShopController@show')->name('shop.detail');
+Route::get('/shop/edit/{id}', 'ShopController@edit')->name('shop.edit');
+Route::post('/shop/update/{id}', 'ShopController@update')->name('shop.update');
 
+Route::get('/shop/{id}', 'ShopController@show')->name('shop.detail');
+Route::delete('/shop/{id}', 'ShopController@destroy')->name('shop.destroy');
 Route::get('/', function () {
     return redirect('/shops');
 });
