e2f832b26f423e1ef07d5b713865ca0282690091
lies046 <skytree.skysky@gmail.com>
2020-07-07 09:46:35 +0900

    Add layout.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,13 @@
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="UTF-8">
-    <title>Lunchmap</title>
-    <style>body {padding: 10px;}</style>
-  </head>
-  <body>
-    <h1>お店一覧</h1>
+@extends('layout')
 
-    @foreach($shops as $shop)
-      <p>
-        {{ $shop->category->name }},
-        {{ $shop->name }},
-        {{ $shop->address}}
-      </p>
-    @endforeach
-  </body>
-</html>
+@section('content')
+  <h1>お店一覧</h1>
+
+  @foreach($shops as $shop)
+    <p>
+      {{ $shop->category->name }},
+      {{ $shop->name }},
+      {{ $shop->address}}
+    </p>
+  @endforeach
+@endsection
