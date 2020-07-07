b8c3b8e970e64c25d9b4266cc436e17644b4ed86
lies046 <skytree.skysky@gmail.com>
2020-07-07 10:24:57 +0900

    Add Show

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 Route::get('/shops', 'ShopController@index')->name('shop.list');
+Route::get('/shop/{id}', 'ShopController@show')->name('shop.detail');
 
 Route::get('/', function () {
     return redirect('/shops');
